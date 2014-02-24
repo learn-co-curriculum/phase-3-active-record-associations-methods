9fa7602e92247e2622936252332eeb109cac2f9b
Logan Hasson <logan.hasson@gmail.com>
2014-02-24 12:55:17 -0500

    Turn off verbose ActiveRecord::Migration output

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,7 @@ Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| requi
 
 DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
 DB = ActiveRecord::Base.connection
+
+if ENV["PLAYLISTER_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
