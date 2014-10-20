3ad0087574a16987c169022deb7cbaa75805b3aa
ahimmelstoss <ahimmelstoss@gmail.com>
2014-10-20 15:18:49 -0400

    spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["PLAYLISTER_ENV"] = "test"
 require_relative '../config/environment'
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.order = 'default'
 
