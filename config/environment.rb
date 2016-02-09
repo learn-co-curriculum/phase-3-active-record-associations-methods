fb1ef325a55c4cbc96bff2e43f93e0cfaf2fa995
Avi Flombaum <avi@flombaum.com>
2016-02-08 22:58:41 -0500

    Fix migration load order

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ def migrate_db
     DB.execute("DROP TABLE #{table}")
   end
 
-  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].each do |f| 
+  Dir[File.join(File.dirname(__FILE__), "../db/migrate", "*.rb")].sort.each do |f| 
     require f
     migration = Kernel.const_get(f.split("/").last.split(".rb").first.gsub(/\d+/, "").split("_").collect{|w| w.strip.capitalize}.join())
     migration.migrate(:up)
