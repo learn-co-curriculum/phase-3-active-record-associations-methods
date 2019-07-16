2919b12ee82a297e3adf42d3f6d491bece54da87
maxwellbenton <maxwellbenton@gmail.com>
2019-07-16 13:09:15 -0400

    updates test for song#drake method

--- spec/models/09_song_methods_spec.rb
+++ spec/models/09_song_methods_spec.rb
@@ -13,7 +13,23 @@ describe 'Song Methods' do
   end
 
   it '#drake_made_this' do
-    expect(@song.drake_made_this).to be_a(Artist)
-    expect(@song.drake_made_this.name).to eq("Drake")
+    expect(@song.artist).to eq(nil)
+
+    @song.drake_made_this
+
+    expect(@song.artist).to be_a(Artist)
+    expect(@song.artist.name).to eq("Drake")
+
+    expect(Artist.all.length).to eq(1)
+
+    kiki = Song.create(name: 'In My Feelings')
+
+    kiki.drake_made_this
+    
+    expect(kiki.artist).to be_a(Artist)
+    expect(kiki.artist.name).to eq("Drake")
+
+    expect(Artist.all.length).to eq(1), 'Create only one Drake artist record in the database, even if method is run multiple times'
+    
   end
 end
