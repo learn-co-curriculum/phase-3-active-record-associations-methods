b2e72f7f1ec06ba08a9b4d2e0715c2b75cab6c7d
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-31 16:12:51 +0300

    commit tree 4a0633babb943501f0ad5a839a0e61f76eea5ddf

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,18 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    newArray = []  
+    self.artists.each do |artist|
+      newArray.push(artist.name)
+    end
+    newArray
   end
 end
